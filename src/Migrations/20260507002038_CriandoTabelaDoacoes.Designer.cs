b234ab4b7c73c5bb1e704cd589c3283bd48ee55b
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-05-06 21:22:25 -0300

    add DB doeações

--- src/Migrations/20260507002038_CriandoTabelaDoacoes.Designer.cs
+++ src/Migrations/20260507002038_CriandoTabelaDoacoes.Designer.cs
@@ -4,6 +4,7 @@ using ConexaoSolidaria.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ConexaoSolidaria.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260507002038_CriandoTabelaDoacoes")]
+    partial class CriandoTabelaDoacoes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Migrations/20260507002038_CriandoTabelaDoacoes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConexaoSolidaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoTabelaDoacoes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DoadorId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    SolicitacaoId = table.Column<int>(type: "int", nullable: false),
+                    ItensDoados = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataDoacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Doacoes_AspNetUsers_DoadorId",
+                        column: x => x.DoadorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Doacoes_Solicitacoes_SolicitacaoId",
+                        column: x => x.SolicitacaoId,
+                        principalTable: "Solicitacoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doacoes_DoadorId",
+                table: "Doacoes",
+                column: "DoadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doacoes_SolicitacaoId",
+                table: "Doacoes",
+                column: "SolicitacaoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doacoes");
+        }
+    }
+}
