a0a03b10e67718f7abe8a5cb71a1c1e0a764f52f
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-05-09 15:04:51 -0300

    Att Hub SignalR

---
+++ src/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ConexaoSolidaria.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task EnviarMensagem(string chatId, string usuario, string mensagem)
+        {
+            // Envia a mensagem para todos que estiverem conectados nesse "chatId"
+            await Clients.Group(chatId).SendAsync("ReceberMensagem", usuario, mensagem);
+        }
+
+        // colocar o usuário na sala correta do chat
+        public async Task EntrarNoChat(string chatId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, chatId);
+        }
+    }
+}
