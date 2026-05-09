6e9b4e90a5e793fa3608a460f26d38ad7067009c
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-05-09 16:33:20 -0300

    Add recuperar senha

---
+++ src/Services/SmtpEmailService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ConexaoSolidaria.Services
+{
+    public class SmtpEmailService : IEmailService
+    {
+        public async Task EnviarEmailAsync(string emailDestino, string assunto, string mensagemHtml)
+        {
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("suporte@conexaosolidaria.app", "Conexão Solidária"),
+                Subject = assunto,
+                Body = mensagemHtml,
+                IsBodyHtml = true,
+            };
+
+            mailMessage.To.Add(emailDestino);
+
+            // DICA: Para testes, crie uma conta grátis no Mailtrap.io
+            // Depois, quando for para produção, basta trocar estes dados pelos do SendGrid ou Gmail
+            using var smtpClient = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("SEU_USUARIO_MAILTRAP", "SUA_SENHA_MAILTRAP"),
+                EnableSsl = true
+            };
+
+            await smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
