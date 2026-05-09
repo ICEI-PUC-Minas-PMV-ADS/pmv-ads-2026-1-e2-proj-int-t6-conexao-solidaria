536dbe9c066f25109a45f217f07f0acd050e5650
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-05-09 14:57:02 -0300

    Add Chat

---
+++ src/Models/MensagemChat.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConexaoSolidaria.Models
+{
+    public class MensagemChat
+    {
+        public int Id { get; set; }
+
+        public int ChatApoioId { get; set; }
+        public virtual ChatApoio? ChatApoio { get; set; }
+
+        [Required]
+        public string RemetenteId { get; set; } = string.Empty;
+        public virtual Usuario? Remetente { get; set; }
+
+        [Required]
+        public string Conteudo { get; set; } = string.Empty;
+
+        public DateTime EnviadaEm { get; set; } = DateTime.Now;
+    }
+}
