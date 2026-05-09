6633234256733cd2467836edf2dc57c57c6edcb3
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-05-09 16:32:41 -0300

    add emails

---
+++ src/Services/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace ConexaoSolidaria.Services
+{
+    public interface IEmailService
+    {
+        Task EnviarEmailAsync(string emailDestino, string assunto, string mensagemHtml);
+    }
+}
