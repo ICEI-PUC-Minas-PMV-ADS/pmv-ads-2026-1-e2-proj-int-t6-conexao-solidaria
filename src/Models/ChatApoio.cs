a55997dceac8198f4d1b3470765359f659285a0d
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-05-09 14:56:33 -0300

    Add

---
+++ src/Models/ChatApoio.cs
@@ -0,0 +1,16 @@
+﻿namespace ConexaoSolidaria.Models
+{
+    public class ChatApoio
+    {
+        public int Id { get; set; }
+
+        // Liga o chat a uma doação específica
+        public int DoacaoId { get; set; }
+        public virtual Doacao? Doacao { get; set; }
+
+        public DateTime CriadoEm { get; set; } = DateTime.Now;
+
+        // Lista de mensagens desta conversa
+        public virtual ICollection<MensagemChat> Mensagens { get; set; } = new List<MensagemChat>();
+    }
+}
