656116addde8bbaabbd5dfba4dc02b117b97971f
Arthur Lobo <arthur.e.a.lobo@gmail.com>
2026-05-09 14:04:12 -0300

    Add banco de dados avaliações

---
+++ src/Migrations/20260509170137_AddAvaliacoes.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConexaoSolidaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAvaliacoes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Avaliacoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DoacaoId = table.Column<int>(type: "int", nullable: false),
+                    Nota = table.Column<int>(type: "int", nullable: false),
+                    Comentario = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    DataAvaliacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AvaliadoId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AvaliadorId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Avaliacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Avaliacoes_AspNetUsers_AvaliadoId",
+                        column: x => x.AvaliadoId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Avaliacoes_AspNetUsers_AvaliadorId",
+                        column: x => x.AvaliadorId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Avaliacoes_Doacoes_DoacaoId",
+                        column: x => x.DoacaoId,
+                        principalTable: "Doacoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacoes_AvaliadoId",
+                table: "Avaliacoes",
+                column: "AvaliadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacoes_AvaliadorId",
+                table: "Avaliacoes",
+                column: "AvaliadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Avaliacoes_DoacaoId",
+                table: "Avaliacoes",
+                column: "DoacaoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Avaliacoes");
+        }
+    }
+}
